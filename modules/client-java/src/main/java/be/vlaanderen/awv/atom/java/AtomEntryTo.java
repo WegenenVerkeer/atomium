042306ca5ffaa6c66c38570026c02f23394f136d
joachimvda <joachim@progs.be>
2014-09-30 14:42:45 +0200

    Add classes for processing in Java and more tests/samples

---
+++ modules/client-java/src/main/java/be/vlaanderen/awv/atom/java/AtomEntryTo.java
@@ -0,0 +1,44 @@
+/*
+ * Dit bestand is een onderdeel van AWV DistrictCenter.
+ * Copyright (c) AWV Agentschap Wegen en Verkeer, Vlaamse Gemeenschap
+ */
+
+package be.vlaanderen.awv.atom.java;
+
+import be.vlaanderen.awv.atom.Entry;
+import be.vlaanderen.awv.atom.Link;
+import lombok.Data;
+import scala.collection.JavaConverters;
+import scala.collection.immutable.List;
+
+import java.util.ArrayList;
+
+/**
+ * Representation of an entry in an atom feed.
+ *
+ * @param <T> entry data type
+ */
+@Data
+public class AtomEntryTo<T> {
+
+    private AtomEntryContentTo<T> content;
+    private FeedLinkTo[] links;
+
+    /**
+     * Converteer naar object dat bruikbaar is voor Atomium.
+     *
+     * @return atomium feed
+     */
+    public Entry<T> toAtomium() {
+        return new Entry<T>(content.toAtomium(), toFeedLinks(links));
+    }
+
+    private List<Link> toFeedLinks(FeedLinkTo[] linkTos) {
+        java.util.List<Link> list = new ArrayList<Link>();
+        for (FeedLinkTo link : linkTos) {
+            list.add(link.toAtomium());
+        }
+        return JavaConverters.asScalaBufferConverter(list).asScala().toList();
+    }
+
+}
---
+++ modules/client-java/src/test/java/be/vlaanderen/awv/atom/java/JacksonParseAtomFeedToScalaObjectTest.java
@@ -0,0 +1,44 @@
+/*
+ * Dit bestand is een onderdeel van AWV DistrictCenter.
+ * Copyright (c) AWV Agentschap Wegen en Verkeer, Vlaamse Gemeenschap
+ */
+
+package be.vlaanderen.awv.atom.java;
+
+import be.vlaanderen.awv.atom.Feed;
+import org.apache.commons.io.FileUtils;
+import org.assertj.core.api.Assertions;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Test JSON parsing van de Atom feed (met generisch entry type).
+ */
+public class JacksonParseAtomFeedToScalaObjectTest {
+
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    @Ignore
+    public void testParseAtomJson() throws Exception {
+        String json = FileUtils.readFileToString(
+                new File(this.getClass().getClassLoader().getResource(
+                        "be/vlaanderen/awv/atom/java/atom-feed-sample.txt").getFile()));
+
+        Feed<EventFeedEntryTo> feed = mapper.readValue(json, new TypeReference<Feed<EventFeedEntryTo>>() {
+        });
+
+        System.out.println(feed.entries().head().content().rawType());
+        System.out.println(feed.entries().head().content().value().head().getClass());
+        System.out.println(feed.entries().head().content().value().head());
+        assertThat(feed).isNotNull();
+        Assertions.assertThat(feed.entries().head().content().value().head()).isInstanceOf(EventFeedEntryTo.class);
+    }
+
+}
