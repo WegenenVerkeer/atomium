c7c4729327eaf6cdcfa88c1fe7f9d17db41f745f
Renato Cavalcanti <renato@strongtyped.io>
2014-12-02 22:26:36 +0100

    AT-9: Catch on NonFatal instead of Exception + we should not log the exception because it's being propagate (encapsulated in a Failure) + Using AnyJson extractor to increase clarity on pattern matching

--- modules/client-scala/src/main/scala/be/vlaanderen/awv/atom/package.scala
+++ modules/client-scala/src/main/scala/be/vlaanderen/awv/atom/package.scala
@@ -2,6 +2,7 @@ package be.vlaanderen.awv
 
 import be.vlaanderen.awv.atom.Marshallers.{JsonUnmarshaller, XmlUnmarshaller}
 
+import scala.util.control.NonFatal
 import scala.util.{Failure, Success, Try}
 
 package object atom {
@@ -15,18 +16,26 @@ package object atom {
     def jsonUnmarshaller: JsonUnmarshaller[Feed[T]]
     def xmlUnmarshaller: XmlUnmarshaller[Feed[T]]
 
+
+
     def unmarshal(contentType: Option[String], body: String): Try[Feed[T]] = {
       try {
         contentType match {
-          case Some(contentType) if contentType.startsWith("application/json") => Success(jsonUnmarshaller(body))
+          case AnyJson(_) => Success(jsonUnmarshaller(body))
           case _ => Success(xmlUnmarshaller(body))
         }
       } catch {
-        case e: Exception =>
-          e.printStackTrace()
-          //TODO log it
+        case NonFatal(e) =>
           Failure(new FeedProcessingException(None, s"problem unmarshalling feed : ${e.getMessage}"))
       }
+    }
+
+    object AnyJson {
+      def unapply(contentTypeOpt: Some[String]): Option[String] =
+        if (contentTypeOpt.get.startsWith("application/json"))
+          contentTypeOpt
+        else
+          None
     }
 
   }
