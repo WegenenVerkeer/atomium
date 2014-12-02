6a94150135062ab362f5e247758165b7783d83fc
Renato Cavalcanti <renato@strongtyped.io>
2014-12-02 22:42:46 +0100

    AT-9: just Try it! (also removing logging since we are propagating the exception)

--- modules/client-java/src/main/scala/be/vlaanderen/awv/atom/java/EntryConsumerWrapper.scala
+++ modules/client-java/src/main/scala/be/vlaanderen/awv/atom/java/EntryConsumerWrapper.scala
@@ -2,9 +2,7 @@ package be.vlaanderen.awv.atom.java
 
 import be.vlaanderen.awv.atom
 import be.vlaanderen.awv.atom._
-import com.typesafe.scalalogging.slf4j.Logging
-
-import scala.util.{Success, Failure}
+import scala.util.Try
 
 /**
  * Wrapper around the [[be.vlaanderen.awv.atom.EntryConsumer]] that offers a Java-like interface.
@@ -12,16 +10,9 @@ import scala.util.{Success, Failure}
  * @param underlying the underlying [[be.vlaanderen.awv.atom.EntryConsumer]]
  * @tparam E the type of the entries in the feed
  */
-class EntryConsumerWrapper[E](underlying: java.EntryConsumer[E]) extends atom.EntryConsumer[E] with Logging {
+class EntryConsumerWrapper[E](underlying: java.EntryConsumer[E]) extends atom.EntryConsumer[E] {
 
-  override def apply(position: FeedPosition, entry: Entry[E]): FeedProcessingResult = {
-    try {
-      underlying.accept(position, entry)
-      Success()
-    } catch {
-      case ex:Exception =>
-        logger.error(s"Error during entry consumption [$entry]", ex)
-        Failure(FeedProcessingException(Option(position), ex.getMessage))
-    }
-  }
+  override def apply(position: FeedPosition, entry: Entry[E]): FeedProcessingResult =
+    Try(underlying.accept(position, entry))
+
 }
