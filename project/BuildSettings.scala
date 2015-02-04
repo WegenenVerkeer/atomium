849c198e9a0c587ee73b004a7bf17d0471f48bb8
Renato Cavalcanti <renato@strongtyped.io>
2015-02-04 15:35:44 +0100

    simplified build + server-play build fixed

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -1,19 +1,14 @@
-import sbt._
-import sbt.Configuration
+import de.johoop.jacoco4sbt.JacocoPlugin._
 import sbt.Keys._
-import de.johoop.jacoco4sbt._
-import JacocoPlugin._
+import sbt.{Configuration, _}
+
 import scala.util.Properties
 
 trait BuildSettings {
 
   import Dependencies._
 
-  val ScalaBuildOptions = Seq("-unchecked", "-deprecation", "-feature",
-    "-language:reflectiveCalls",
-    "-language:implicitConversions",
-    "-language:postfixOps")
-
+  val projectName = "atomium"
 
   def scalaTestOptions(config: Configuration) = inConfig(config)(Seq(
     testOptions += Tests.Argument("-F", Properties.envOrElse("SCALED_TIME_SPAN", "1"))
@@ -23,68 +18,97 @@ trait BuildSettings {
     libraryDependencies ++= mainTestDependencies
   ) ++ scalaTestOptions(Test) ++ scalaTestOptions(jacoco.Config)
 
-  
-  def projectSettings(projectName:String, extraDependencies:Seq[ModuleID]) = Seq(
-    name := projectName,
-    parallelExecution := false,
-    resolvers +=  "Local Maven" at Path.userHome.asFile.toURI.toURL + ".m2/repository",
-    resolvers += Resolver.typesafeRepo("releases"),
-    libraryDependencies ++= mainDependencies ++ extraDependencies
-  )
 
-  val publishingCredentials = (for {
-    username <- Option(System.getenv().get("SONATYPE_USERNAME"))
-    password <- Option(System.getenv().get("SONATYPE_PASSWORD"))
-  } yield
-    Seq(Credentials(
-      "Sonatype Nexus Repository Manager",
-      "oss.sonatype.org",
-      username,
-      password)
-    )).getOrElse(Seq())
+  def project(moduleName: String): Project = {
+    Project(
+      id = projectName + "-" + moduleName,
+      base = file("modules/" + moduleName),
+      settings = projectSettings()
+    )
+  }
 
 
-  val publishSettings = Seq(
-    publishMavenStyle := true,
-    pomIncludeRepository := { _ => false},
-    publishTo := {
+  def mainProject(modules: ProjectReference*): Project = {
+    Project(
+      id = projectName,
+      base = file("."),
+      settings = projectSettings()
+    )
+    .settings(publishArtifact := false)
+    .aggregate(modules: _*)
+  }
+
+  private def projectSettings() = {
+
+    val projectSettings = Seq(
+      parallelExecution := false,
+      resolvers += "Local Maven" at Path.userHome.asFile.toURI.toURL + ".m2/repository",
+      resolvers += Resolver.typesafeRepo("releases"),
+      libraryDependencies ++= mainDependencies
+    )
+
+    Defaults.coreDefaultSettings ++ projectSettings ++ testSettings ++ publishSettings ++ jacoco.settings
+  }
+
+  val publishSettings = {
+
+    val publishingCredentials = {
+
+      val credentials =
+        for {
+          username <- Option(System.getenv().get("SONATYPE_USERNAME"))
+          password <- Option(System.getenv().get("SONATYPE_PASSWORD"))
+        } yield
+          Credentials(
+            "Sonatype Nexus Repository Manager",
+            "oss.sonatype.org",
+            username,
+            password
+          )
+
+      credentials.toSeq
+    }
+
+    // explicitly creating a Def to avoid cluttered code inside when adding to Seq bellow
+    val repoToPublish = Def setting {
       val nexus = "https://oss.sonatype.org/"
       if (isSnapshot.value)
         Some("snapshots" at nexus + "content/repositories/snapshots")
       else
         Some("releases" at nexus + "service/local/staging/deploy/maven2")
-    },
-    pomExtra := pomInfo,
-    credentials ++= publishingCredentials
-  )
+    }
 
-  def buildSettings(projectName:String, extraDependencies:Seq[ModuleID] = Seq()) = {
-    Defaults.defaultSettings ++
-      projectSettings(projectName, extraDependencies) ++
-      testSettings ++
-      publishSettings ++
-      jacoco.settings
+
+    val pomInfo = {
+      <url>https://github.com/WegenenVerkeer/atomium</url>
+        <licenses>
+          <license>
+            <name>MIT licencse</name>
+            <url>http://opensource.org/licenses/MIT</url>
+            <distribution>repo</distribution>
+          </license>
+        </licenses>
+        <scm>
+          <url>git@github.com:WegenenVerkeer/atomium.git</url>
+          <connection>scm:git:git@github.com:WegenenVerkeer/atomium.git</connection>
+        </scm>
+        <developers>
+          <developer>
+            <id>AWV</id>
+            <name>De ontwikkelaars van AWV</name>
+            <url>http://www.wegenenverkeer.be</url>
+          </developer>
+        </developers>
+    }
+
+    Seq(
+      publishMavenStyle := true,
+      pomIncludeRepository := { _ => false},
+      publishTo := repoToPublish.value,
+      pomExtra := pomInfo,
+      credentials ++= publishingCredentials
+    )
   }
-
-  lazy val pomInfo = <url>https://github.com/WegenenVerkeer/atomium</url>
-      <licenses>
-        <license>
-          <name>MIT licencse</name>
-          <url>http://opensource.org/licenses/MIT</url>
-          <distribution>repo</distribution>
-        </license>
-      </licenses>
-      <scm>
-        <url>git@github.com:WegenenVerkeer/atomium.git</url>
-        <connection>scm:git:git@github.com:WegenenVerkeer/atomium.git</connection>
-      </scm>
-      <developers>
-        <developer>
-          <id>AWV</id>
-          <name>De ontwikkelaars van AWV</name>
-          <url>http://www.wegenenverkeer.be</url>
-        </developer>
-      </developers>
 
 
 }
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,38 +6,39 @@ object Dependencies {
   val playVersion = "2.3.6"
 
   // main deps
-  val logback           = "ch.qos.logback"          %     "logback-classic"         % "1.1.1"
-  val jodaTime          = "joda-time"               %     "joda-time"               % "2.3"
-  val jodaConvert       = "org.joda"                %     "joda-convert"            % "1.2"
-  val playJson          = "com.typesafe.play"       %%    "play-json"               % playVersion
-  val playWs            = "com.typesafe.play"       %%    "play-ws"                 % playVersion
-  val play              = "com.typesafe.play"       %%    "play"                    % playVersion
-  val mongoJavaDriver   = "org.mongodb"             %     "mongo-java-driver"       % "2.0"
-  val casbah            = "org.mongodb"             %%    "casbah"                  % "2.8.0"
-  val slick             = "com.typesafe.slick"      %%    "slick"                   % "2.1.0"
-  val slickPostgres     = "com.github.tminglei"     %%    "slick-pg"                % "0.7.0"
+  val logback           = "ch.qos.logback"              %     "logback-classic"         % "1.1.1"
+  val jodaTime          = "joda-time"                   %     "joda-time"               % "2.3"
+  val jodaConvert       = "org.joda"                    %     "joda-convert"            % "1.2"
+  val playJson          = "com.typesafe.play"           %%    "play-json"               % playVersion
+  val playWs            = "com.typesafe.play"           %%    "play-ws"                 % playVersion
+  val play              = "com.typesafe.play"           %%    "play"                    % playVersion
+  val mongoJavaDriver   = "org.mongodb"                 %     "mongo-java-driver"       % "2.0"
+  val casbah            = "org.mongodb"                 %%    "casbah"                  % "2.8.0"
+  val slick             = "com.typesafe.slick"          %%    "slick"                   % "2.1.0"
+  val slickPostgres     = "com.github.tminglei"         %%    "slick-pg"                % "0.7.0"
+  val scalaLogging      = "com.typesafe.scala-logging"  %%    "scala-logging"           % "3.1.0"
 
   // test deps
-  val scalaTest     = "org.scalatest"               %%    "scalatest"               % "2.2.0"        % "test"
-  val scalaTestPlay = "org.scalatestplus"           %%    "play"                    % "1.2.0"        % "test"
-  val scalaCheck    = "org.scalacheck"              %%    "scalacheck"              % "1.12.1"       % "test"
-  val embededMongo  = "com.github.simplyscala"      %%    "scalatest-embedmongo"    % "0.2.2"        % "test"
-  val h2database    = "com.h2database"              %     "h2"                      % "1.0.60"       % "test"
-  val playMockWs    = "de.leanovate.play-mockws"    %%    "play-mockws"             % "0.12"         % "test"
-  val playTest      = "com.typesafe.play"           %%    "play-test"               % playVersion    % "test"
+  val scalaTest         = "org.scalatest"               %%    "scalatest"               % "2.2.0"        % "test"
+  val scalaTestPlay     = "org.scalatestplus"           %%    "play"                    % "1.2.0"        % "test"
+  val scalaCheck        = "org.scalacheck"              %%    "scalacheck"              % "1.12.1"       % "test"
+  val embededMongo      = "com.github.simplyscala"      %%    "scalatest-embedmongo"    % "0.2.2"        % "test"
+  val h2database        = "com.h2database"              %     "h2"                      % "1.0.60"       % "test"
+  val playMockWs        = "de.leanovate.play-mockws"    %%    "play-mockws"             % "0.12"         % "test"
+  val playTest          = "com.typesafe.play"           %%    "play-test"               % playVersion    % "test"
 
   // java deps
-  val junit           = "junit"                           %   "junit"                   % "4.11"    % "test"
-  val lombok          = "org.projectlombok"               %   "lombok"                  % "1.14.4"  % "test"
-  val jacksonDatabind = "com.fasterxml.jackson.core"      %   "jackson-databind"        % "2.4.3"
-  val jacksonJoda     = "com.fasterxml.jackson.datatype"  %   "jackson-datatype-joda"   % "2.4.3"
+  val junit             = "junit"                           %   "junit"                   % "4.11"    % "test"
+  val lombok            = "org.projectlombok"               %   "lombok"                  % "1.14.4"  % "test"
+  val jacksonDatabind   = "com.fasterxml.jackson.core"      %   "jackson-databind"        % "2.4.3"
+  val jacksonJoda       = "com.fasterxml.jackson.datatype"  %   "jackson-datatype-joda"   % "2.4.3"
 
 
-  val slf4j           = "org.slf4j"                       %   "slf4j-api"               % "1.7.6"
-  val mockitoCore     = "org.mockito"                     %   "mockito-core"            % "1.9.5"   % "test"
-  val assertJ         = "org.assertj"                     %   "assertj-core"            % "1.5.0"   % "test"
-  val jfakerMockito   = "be.eliwan"                       %   "jfaker-mockito"          % "0.1"     % "test"
-  val commonsIo       = "commons-io"                      %   "commons-io"              % "2.4"     % "test"
+  val slf4j             = "org.slf4j"                       %   "slf4j-api"               % "1.7.6"
+  val mockitoCore       = "org.mockito"                     %   "mockito-core"            % "1.9.5"   % "test"
+  val assertJ           = "org.assertj"                     %   "assertj-core"            % "1.5.0"   % "test"
+  val jfakerMockito     = "be.eliwan"                       %   "jfaker-mockito"          % "0.1"     % "test"
+  val commonsIo         = "commons-io"                      %   "commons-io"              % "2.4"     % "test"
 
 
   val mainDependencies = Seq(
@@ -48,26 +49,8 @@ object Dependencies {
 
   val mainTestDependencies = Seq (
     scalaTest,
-    scalaCheck,
-    embededMongo,
-    h2database,
-    playMockWs,
-    playTest
+    scalaCheck
   )
 
-  val commonPlayDependencies = Seq (
-    play,
-    playJson
-  )
 
-  val clientPlayDependencies = Seq (
-    playWs
-  )
-
-  val javaDependencies = Seq (
-    junit,
-    lombok,
-    jacksonDatabind,
-    jacksonJoda
-  )
 }
