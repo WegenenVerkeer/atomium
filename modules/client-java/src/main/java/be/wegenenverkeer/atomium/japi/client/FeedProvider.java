a02fed5d0c095321586997651dc51ffa5ad6214a
Renato Cavalcanti <renato@strongtyped.io>
2015-02-18 14:17:20 +0100

    removed wrong documentation fragment

--- modules/client-java/src/main/java/be/wegenenverkeer/atomium/japi/client/FeedProvider.java
+++ modules/client-java/src/main/java/be/wegenenverkeer/atomium/japi/client/FeedProvider.java
@@ -6,11 +6,6 @@ import be.wegenenverkeer.atomium.japi.format.Feed;
 /**
  * A feed provider is responsible for providing the feed pages.
  *
- * Currently, Atomium comes with 1 implementation out-of-the-box, a provider that uses the Play WS API for fetching feed
- * pages via HTTP, [[be.wegenenverkeer.atom.providers.PlayWsBlockingFeedProvider]].
- *
- * When fetching the feed pages, a feed provider should return a [[scala.util.Try]] instead of throwing an exception.
- *
  * @param <E> the type of the entries in the feed
  */
 public interface FeedProvider<E> {
