88943cde96b6f7a2b8ee7940fd1b8433b7fb0721
Ben Cochez <ben@cochez.consulting>
2020-06-23 21:54:17 +0200

    test to demonstrate eager fetching issue

--- modules/client-java/src/test/java/be/wegenenverkeer/atomium/client/BackpressureTest.java
+++ modules/client-java/src/test/java/be/wegenenverkeer/atomium/client/BackpressureTest.java
@@ -7,6 +7,7 @@ import com.github.tomakehurst.wiremock.common.ClasspathFileSource;
 import com.github.tomakehurst.wiremock.common.Slf4jNotifier;
 import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -14,14 +15,13 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static be.wegenenverkeer.atomium.client.FeedPositionStrategies.from;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.exactly;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class BackpressureTest {
     private final static ClasspathFileSource WIREMOCK_MAPPINGS = new ClasspathFileSource("basis-scenario");
@@ -63,6 +63,25 @@ public class BackpressureTest {
                 .awaitCount(3)
                 .assertNoErrors()
                 .assertValueCount(3);
+
+        // only 1 page is queried
+        WireMock.verify(exactly(1), WireMock.getRequestedFor(WireMock.urlPathEqualTo("/feeds/events/20/forward/10")).withHeader("Accept", equalTo("application/xml")));
+        WireMock.verify(exactly(0), WireMock.getRequestedFor(WireMock.urlPathEqualTo("/feeds/events/30/forward/10")).withHeader("Accept", equalTo("application/xml")));
+        WireMock.verify(exactly(0), WireMock.getRequestedFor(WireMock.urlPathEqualTo("/feeds/events/40/forward/10")).withHeader("Accept", equalTo("application/xml")));
+        WireMock.verify(exactly(0), WireMock.getRequestedFor(WireMock.urlPathEqualTo("/feeds/events/50/forward/10")).withHeader("Accept", equalTo("application/xml")));
+    }
+
+    @Test
+    public void testProcessing_otherThread() {
+        List<FeedEntry<Event>> entries = client.feed(client.getPageFetcherBuilder("/feeds/events", Event.class).setAcceptXml().build())
+                .fetchEntries(from("20/forward/10", "urn:uuid:8641f2fd-e8dc-4756-acf2-3b708080ea3a"))
+                .concatMap(event -> Flowable.just(event)
+                        .doOnNext(myEvent -> Thread.sleep(Duration.ofSeconds(1).toMillis()))
+                        .subscribeOn(Schedulers.newThread()), 1)
+                .test()
+                .awaitDone(3, TimeUnit.SECONDS)
+                .assertNoErrors()
+                .values();
 
         // only 1 page is queried
         WireMock.verify(exactly(1), WireMock.getRequestedFor(WireMock.urlPathEqualTo("/feeds/events/20/forward/10")).withHeader("Accept", equalTo("application/xml")));
