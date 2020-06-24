447cf6d904915ad32f725923046ebc3c2b27a60d
Ben Cochez <ben@cochez.consulting>
2020-06-24 13:08:57 +0200

    add debug logging

--- modules/client-java/src/main/java/be/wegenenverkeer/atomium/client/AtomiumFeed.java
+++ modules/client-java/src/main/java/be/wegenenverkeer/atomium/client/AtomiumFeed.java
@@ -1,7 +1,9 @@
 package be.wegenenverkeer.atomium.client;
 
 import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,14 +30,22 @@ public class AtomiumFeed<E> {
 
     public Flowable<FeedEntry<E>> fetchEntries(CachedFeedPage<E> currentPage, FeedPositionStrategy feedPositionStrategy, Optional<String> eTag) {
         return feedPositionStrategy.getNextFeedPosition(currentPage)
+                .doOnEvent((feedPosition, throwable) -> logger.debug("Next feed position = {} (page {})", feedPosition.getEntryId(), feedPosition.getPageUrl()))
                 .flatMap(feedPosition -> fetchPage(feedPosition.getPageUrl(), eTag)
                         .map(page -> ParsedFeedPage.parse(page, feedPosition))
                 )
+                .doOnEvent((page, throwable) -> logger.debug("Page fetched {}", page.getPage().getSelfHref()))
                 .toFlowable()
-                .flatMap(parsedPage -> Flowable.fromIterable(parsedPage.getEntries()).concatWith(
-                        fetchEntries(parsedPage.getPage(), feedPositionStrategy, eTag)
-                ));
+                .flatMap(parsedPage -> Flowable
+                        .fromIterable(parsedPage.getEntries())
+                        .doOnNext(eFeedEntry -> logger.debug("Returning feed entry {} (page {})", eFeedEntry.getEntry().getId(), eFeedEntry.getSelfHref()))
+                        .concatWith(fetchEntries(parsedPage.getPage(), feedPositionStrategy, eTag)
+                ))
+                .doOnNext(eFeedEntry -> logger.debug("Returning feed entry {} (page {})", eFeedEntry.getEntry().getId(), eFeedEntry.getSelfHref()))
+                ;
     }
+
+
 
     private Single<CachedFeedPage<E>> fetchHeadPage() {
         return fetchPage(AtomiumFeed.FIRST_PAGE, Optional.empty());
